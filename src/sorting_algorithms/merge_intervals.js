5a667fdeecab3e153e3bbf17c121e554d8cfc9cb
Victor Johnson <johnsonsirv@gmail.com>
2023-05-03 13:16:39 +0100

    add mergeIntervalsOptimised for space and time

--- src/sorting_algorithms/merge_intervals.js
+++ src/sorting_algorithms/merge_intervals.js
@@ -6,10 +6,10 @@ function overlap(arr1, arr2) {
     return false
 }
 
-function mergeIntervals(arr) {
+function mergeIntervals(intervals) {
     // sort the array so overlapping intervals wil be adjacent
-    const sorted = arr.sort((a, b) => a[0] - b[0]) // sort ascending
-    const result = [arr[0]];
+    const sorted = intervals.sort((a, b) => a[0] - b[0]) // sort ascending
+    const result = [sorted[0]];
 
     for (let i = 1; i < sorted.length; i++) {
         const current = sorted[i];
@@ -26,8 +26,44 @@ function mergeIntervals(arr) {
     return result;
 }
 
+
+// Optimized for time - by removing the helper function & return early
+// Optimized for space - by sorting original array in place
+function mergedIntervalsOptimized(intervals) {
+    // Clarifying questions
+    // What is considered overlapping? Is having the same start as and end considered overlapping?
+    // Are the intervals sorted by start value? 
+    // How many intervals? 
+
+    // Handle base cases
+    if (intervals == null) {
+        return null;
+    } else if (intervals.length === 0) {
+        return [];
+    }
+
+    // Sorting solution
+    // O(NlogN) time complexity since we first sort the N intervals, then iterate over them to create the merged arrays
+    // O(logN) space complexity for the sorting algorithm. Input and output arrays are not included in extra space complexity. Constant number of variables in addition
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const merged = [];
+    merged.push(intervals[0]);
+    for (let i = 1; i < intervals.length; i++) {
+        const last = merged[merged.length-1];
+        const current = intervals[i];
+        if (current[0] <= last[1]) {
+            last[1] = Math.max(last[1], current[1]);
+        } else {
+            merged.push(current);
+        }
+    }
+
+    return merged;    
+}
+
 const arr = [[1, 3], [2, 6], [8, 10], [15, 18]]
-console.log(mergeIntervals(arr)); // [[1,6], [8,10], [15,18]]
-console.log(mergeIntervals([[0,3], [2,5]])); // [[0, 5]]
-console.log(mergeIntervals([[0,3], [2,5], [6,7]])); // [[0, 5], [6,7]]
-console.log(mergeIntervals([[4,5], [3,4], [0,1], [1,2], [2,3]])); // [[0, 4]]]
+console.log(mergedIntervalsOptimized(arr)); // [[1,6], [8,10], [15,18]]
+console.log(mergedIntervalsOptimized([[0,3], [2,5]])); // [[0, 5]]
+console.log(mergedIntervalsOptimized([[0,3], [2,5], [6,7]])); // [[0, 5], [6,7]]
+console.log(mergedIntervalsOptimized([[4,5], [3,4], [0,1], [1,2], [2,3]])); // [[0, 4]]]
